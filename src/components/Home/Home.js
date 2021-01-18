1648d76dcfbc2cd67f7502a01eec6e5938102ec6
parthmodi152 <modi.parth152@gmail.com>
2021-01-18 20:14:07 +0530

    final app front-end

---
+++ src/components/Home/Home.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import { Container, Grow, Grid } from "@material-ui/core";
+import useStyles from "./styles";
+import { getPosts } from "../../actions/posts";
+import { useDispatch } from "react-redux";
+
+function Home() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const [currentId, setCurrentId] = useState(0);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+  return (
+    <Grow in>
+      <Container>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+          className={classes.mainContainer}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+}
+
+export default Home;
